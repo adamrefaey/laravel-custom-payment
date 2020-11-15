00ced5751bc1a75327efa49ef124788a90b18402
Mustafa Refaey <mustafarefaey@gmail.com>
2020-11-15 10:05:17 +0200

    add add_payment_handler_command_should_add_an_entry_in_config test

--- tests/Commands/AddPaymentHandlerCommandTest.php
+++ tests/Commands/AddPaymentHandlerCommandTest.php
@@ -20,8 +20,19 @@ class AddPaymentHandlerCommandTest extends TestCase
         $this->artisan("payment:add-handler", ['--name' => $name, '--class' => $class])->assertExitCode(0);
 
         $paymentConfig = require config_path('laravel-custom-payment.php');
-        $handlersNamespace = $paymentConfig['handlers_namespace'];
+        $handlersNamespace = config('core-config.handlers_namespace');
         $this->assertArrayHasKey($name, $paymentConfig['handlers']);
         $this->assertEquals($handlersNamespace . "\\" . $class, $paymentConfig['handlers'][$name]);
+    }
+
+    /** @test */
+    public function add_payment_handler_command_should_scaffold_a_class_in_handlers_directory()
+    {
+        $name = 'some_payment_handler';
+        $class = 'SomePaymentHandler';
+        $this->artisan("payment:add-handler", ['--name' => $name, '--class' => $class])->assertExitCode(0);
+
+        $paymentConfig = require config_path('laravel-custom-payment.php');
+        $this->assertFileExists(base_path(config('core-config.handlers_directory') . '/' . $class . '.php'));
     }
 }

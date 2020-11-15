6dc3fd89093539612035560f47c7a74640119c33
Mustafa Refaey <mustafarefaey@gmail.com>
2020-11-15 10:06:00 +0200

    add payment handler command scaffold a class

--- src/Commands/AddPaymentHandlerCommand.php
+++ src/Commands/AddPaymentHandlerCommand.php
@@ -12,8 +12,23 @@ class AddPaymentHandlerCommand extends Command
 
     public function handle()
     {
+        $name = $this->option('name');
+        $class = $this->option('class');
+
+        // add entry to config
         $paymentConfig = require config_path('laravel-custom-payment.php');
-        $paymentConfig['handlers'][$this->option('name')] = $paymentConfig['handlers_namespace'] . "\\" . $this->option('class');
+        $paymentConfig['handlers'][$name] = config('core-config.handlers_namespace') . "\\" . $class;
         file_put_contents(config_path('laravel-custom-payment.php'), '<?php return ' . var_export($paymentConfig, true) . ';');
+
+        // scaffold class from stub into handlers directory
+        $handlerStub = file_get_contents(__DIR__ . '/../../resources/stubs/payment-handler.stub');
+        $handlerStub = str_replace('{{ namespace }}', config('core-config.handlers_namespace'), $handlerStub);
+        $handlerStub = str_replace('{{ class }}', $class, $handlerStub);
+
+        if (!is_dir(base_path(config('core-config.handlers_directory')))) {
+            mkdir(base_path(config('core-config.handlers_directory')), 0777, true);
+        }
+
+        file_put_contents(base_path(config('core-config.handlers_directory') . '/' . $class . '.php'), $handlerStub);
     }
 }

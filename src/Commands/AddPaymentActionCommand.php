19f9e55c1eb2c5508daa9f5d7d4141984e9e1220
Mustafa Refaey <mustafarefaey@gmail.com>
2020-11-17 20:43:38 +0200

    AddPaymentActionCommand scaffolds a class

--- src/Commands/AddPaymentActionCommand.php
+++ src/Commands/AddPaymentActionCommand.php
@@ -19,5 +19,16 @@ class AddPaymentActionCommand extends Command
         $paymentConfig = require config_path('laravel-custom-payment.php');
         $paymentConfig['actions'][$name] = $paymentConfig['actions_namespace'] . "\\" . $class;
         file_put_contents(config_path('laravel-custom-payment.php'), '<?php return ' . var_export($paymentConfig, true) . ';');
+
+        // scaffold class from stub into actions directory
+        $actionStub = file_get_contents(__DIR__ . '/../../resources/stubs/payment-action.stub');
+        $actionStub = str_replace('{{ namespace }}', $paymentConfig['actions_namespace'], $actionStub);
+        $actionStub = str_replace('{{ class }}', $class, $actionStub);
+
+        if (! is_dir(base_path($paymentConfig['actions_directory']))) {
+            mkdir(base_path($paymentConfig['actions_directory']), 0777, true);
+        }
+
+        file_put_contents(base_path($paymentConfig['actions_directory'] . '/' . $class . '.php'), $actionStub);
     }
 }

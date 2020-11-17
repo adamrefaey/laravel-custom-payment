e171fc3270daa9f4b623362603267a518c26ae10
Mustafa Refaey <mustafarefaey@gmail.com>
2020-11-17 20:41:59 +0200

    AddPaymentActionCommand adds an entry to config

--- config/laravel-custom-payment.php
+++ config/laravel-custom-payment.php
@@ -4,4 +4,7 @@ return [
     'handlers_directory' => "app/Payment/Handlers",
     'handlers_namespace' => "App\\Payment\\Handlers",
     'handlers' => [],
+    'actions_directory' => "app/Payment/Actions",
+    'actions_namespace' => "App\\Payment\\Actions",
+    'actions' => [],
 ];
--- src/Commands/AddPaymentActionCommand.php
+++ src/Commands/AddPaymentActionCommand.php
@@ -12,5 +12,12 @@ class AddPaymentActionCommand extends Command
 
     public function handle()
     {
+        $name = $this->option('name');
+        $class = $this->option('class');
+
+        // add entry to config
+        $paymentConfig = require config_path('laravel-custom-payment.php');
+        $paymentConfig['actions'][$name] = $paymentConfig['actions_namespace'] . "\\" . $class;
+        file_put_contents(config_path('laravel-custom-payment.php'), '<?php return ' . var_export($paymentConfig, true) . ';');
     }
 }

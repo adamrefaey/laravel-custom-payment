a4b4b27b73d79b5de24ac2132064c98da830e18f
Mustafa Refaey <mustafarefaey@gmail.com>
2020-11-17 20:42:43 +0200

    add test add_payment_action_command_should_scaffold_a_class_in_actions_directory

--- tests/Commands/AddPaymentActionCommandTest.php
+++ tests/Commands/AddPaymentActionCommandTest.php
@@ -25,4 +25,15 @@ class AddPaymentActionCommandTest extends TestCase
         $this->assertArrayHasKey($name, $paymentConfig['actions']);
         $this->assertEquals($actionsNamespace . "\\" . $class, $paymentConfig['actions'][$name]);
     }
+
+    /** @test */
+    public function add_payment_action_command_should_scaffold_a_class_in_actions_directory()
+    {
+        $name = 'some_payment_action';
+        $class = 'SomePaymentAction';
+        $this->artisan("payment:add-action", ['--name' => $name, '--class' => $class])->assertExitCode(0);
+
+        $paymentConfig = require config_path('laravel-custom-payment.php');
+        $this->assertFileExists(base_path($paymentConfig['actions_directory'] . '/' . $class . '.php'));
+    }
 }
